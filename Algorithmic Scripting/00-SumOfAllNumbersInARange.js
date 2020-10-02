112775b2aa8d1b354fc74b055a942fc07a496800
Udit Sehra <uditsehra@gmail.com>
2020-10-03 01:48:01 +0530

    recursive solution for sum in range

--- Algorithmic Scripting/00-SumOfAllNumbersInARange.js
+++ Algorithmic Scripting/00-SumOfAllNumbersInARange.js
@@ -27,3 +27,26 @@ let sumAll = (arr)=>{
 
 console.log(sumAll([1,4]))
 // Or we can also use Math.max() or Math.min() function to determine which is min or max.
+
+//Another approach will be
+const sumAll = arr => {
+    // Buckle up everything to one!
+    const startNum = arr[0];
+    const endNum = arr[1];
+  
+    // Get the count of numbers between the two numbers by subtracting them and add 1 to the absolute value.
+    // ex. There are |1-4| + 1 = 4, (1, 2, 3, 4), 4 numbers between 1 and 4.
+    const numCount = Math.abs(startNum - endNum) + 1;
+  
+    // Using Arithmetic Progression summing formula
+    const sum = ((startNum + endNum) * numCount) / 2;
+    return sum;
+  };
+
+  //Recursive solution for the same will be
+  function sumAll([ first, last ] ) {
+    const step = first - last < 0 ? 1 : -1;
+    return first !== last
+      ? first + sumAll([ first + step, last ])
+      : first;
+   }
