6b9d87391683d8f62bf3a3e5cf4bd7b5d8aa68af
Udit Sehra <uditsehra@gmail.com>
2020-10-16 11:05:12 +0530

    Drop elemets from array if function return true

---
+++ Algorithmic Scripting/15-DropIt.js
@@ -0,0 +1,23 @@
+/*
+Given the array arr, iterate through and remove each element starting from the first element (the 0 index)
+until the function func returns true when the iterated element is passed through it.
+
+Then return the rest of the array once the condition is satisfied, otherwise, arr should be returned as an empty array.\
+*/
+
+function dropElements(arr, func) {
+    var time = arr.length
+    for(var i=0; i<time; i++){
+        console.log(func(arr[0]))
+        if(func(arr[0])){
+            break
+        }
+        else{
+            arr.shift()
+        }
+    }
+    return arr
+}
+
+var r = dropElements([1, 2, 3], function(n) {return n < 3; });
+console.log(r)
